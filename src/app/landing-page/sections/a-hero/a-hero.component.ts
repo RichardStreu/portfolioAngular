6c3d57bc4354bedfb904aceee6c2ea65738d6317
Richard Streu <rstreu13@icloud.com>
2024-12-02 18:27:03 +0100

    implemented socialMedia links in hero section

--- src/app/landing-page/sections/a-hero/a-hero.component.ts
+++ src/app/landing-page/sections/a-hero/a-hero.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../core/header/header.component";
+import { ArrowLeftComponent } from './arrow-left/arrow-left.component';
+import { ArrowRightComponent } from './arrow-right/arrow-right.component';
 
 @Component({
   selector: 'app-a-hero',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ArrowLeftComponent, ArrowRightComponent],
   templateUrl: './a-hero.component.html',
   styleUrl: './a-hero.component.scss'
 })
