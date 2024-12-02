fb0d32d268c24709f687019014f7bc7fa0af1062
Richard Streu <rstreu13@icloud.com>
2024-12-02 16:47:46 +0100

    language switch button styling is ready

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  lang: boolean = false;
+
+  switchButton() {
+    this.lang = !this.lang;
+  }
+
 
 }
