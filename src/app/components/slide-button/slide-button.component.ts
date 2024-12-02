871fc8b6a4516bb8ab9d4a131478222475ffdb85
Richard Streu <rstreu13@icloud.com>
2024-12-02 19:56:57 +0100

    created a slide-button component

---
+++ src/app/components/slide-button/slide-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './slide-button.component.html',
+  styleUrl: './slide-button.component.scss'
+})
+export class SlideButtonComponent {
+
+  @Input("text") text: string = "";
+  @Input("path") path: string = "";
+
+}
