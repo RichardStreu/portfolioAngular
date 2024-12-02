b8dc9d5b8ae0be5c0a17930e597db1bc9a0fbdf2
Richard Streu <rstreu13@icloud.com>
2024-12-02 14:02:28 +0100

    imported header component into hero section

--- src/app/landing-page/sections/a-hero/a-hero.component.ts
+++ src/app/landing-page/sections/a-hero/a-hero.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../core/header/header.component";
 
 @Component({
   selector: 'app-a-hero',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './a-hero.component.html',
   styleUrl: './a-hero.component.scss'
 })
