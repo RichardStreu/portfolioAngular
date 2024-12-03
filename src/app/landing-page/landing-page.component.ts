3ae7c5c22f5ee1b8be307c3324624596148b0572
Richard Streu <rstreu13@icloud.com>
2024-12-03 12:21:56 +0100

    about me img + mackground implemented

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AHeroComponent } from './sections/a-hero/a-hero.component';
+import { BAboutmeComponent } from './sections/b-aboutme/b-aboutme.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [AHeroComponent],
+  imports: [AHeroComponent, BAboutmeComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
