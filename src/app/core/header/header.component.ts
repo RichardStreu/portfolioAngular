2e6576cfef8ffa8bd42d967069ca2426ff0b9023
Richard Streu <rstreu13@icloud.com>
2024-12-03 10:53:18 +0100

    implemented a service and function to switch the language between english and german

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,19 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  languageService = inject(LanguageService);
 
   lang: boolean = false;
 
   switchButton() {
     this.lang = !this.lang;
+    this.languageService.switchLanguage();
   }
 
+  aboutMeText: string = 'About Me';
+  skillsText: string = 'Skills';
+  projectsText: string = 'Projects';
 
+  chooseLanguage() {
+    this.languageService.language.subscribe(() => {
+      this.aboutMeText =
+        this.languageService.currentLanguage == 'en' ? 'About Me' : 'Über Mich';
+      this.skillsText =
+        this.languageService.currentLanguage == 'en' ? 'Skills' : 'Fähigkeiten';
+      this.projectsText =
+        this.languageService.currentLanguage == 'en' ? 'Projects' : 'Projekte';
+    });
+  }
+
+  constructor() {
+    this.chooseLanguage();
+  }
 }
--- src/app/landing-page/sections/a-hero/a-hero.component.ts
+++ src/app/landing-page/sections/a-hero/a-hero.component.ts
@@ -1,17 +1,44 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../../../core/header/header.component";
+import { Component, inject } from '@angular/core';
+import { HeaderComponent } from '../../../core/header/header.component';
 import { ArrowLeftComponent } from './arrow-left/arrow-left.component';
 import { ArrowRightComponent } from './arrow-right/arrow-right.component';
 import { SlideButtonComponent } from '../../../components/slide-button/slide-button.component';
 import { SlideBarComponent } from '../../../components/slide-bar/slide-bar.component';
+import { LanguageService } from '../../../services/language.service';
 
 @Component({
   selector: 'app-a-hero',
   standalone: true,
-  imports: [HeaderComponent, ArrowLeftComponent, ArrowRightComponent, SlideButtonComponent, SlideBarComponent],
+  imports: [
+    HeaderComponent,
+    ArrowLeftComponent,
+    ArrowRightComponent,
+    SlideButtonComponent,
+    SlideBarComponent,
+  ],
   templateUrl: './a-hero.component.html',
-  styleUrl: './a-hero.component.scss'
+  styleUrl: './a-hero.component.scss',
 })
 export class AHeroComponent {
+  languageService = inject(LanguageService);
 
+  checkMyWorkText: string = 'Check My Work';
+  contactMeText: string = 'Contact Me';
+
+  chooseLanguage() {
+    this.languageService.language.subscribe(() => {
+      this.checkMyWorkText =
+        this.languageService.currentLanguage == 'en'
+          ? 'Check my Work'
+          : 'Meine Projekte';
+      this.contactMeText =
+        this.languageService.currentLanguage == 'en'
+          ? 'Contact Me'
+          : 'Schreib mir';
+    });
+  }
+
+  constructor() {
+    this.chooseLanguage();
+  }
 }
