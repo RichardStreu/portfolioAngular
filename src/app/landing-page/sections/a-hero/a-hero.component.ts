c90d1d1d3e42a60b79213bfb89e516c1d60375ad
Richard Streu <rstreu13@icloud.com>
2024-12-03 09:18:15 +0100

    installed the slide bar without just animation

--- src/app/landing-page/sections/a-hero/a-hero.component.ts
+++ src/app/landing-page/sections/a-hero/a-hero.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from "../../../core/header/header.component";
 import { ArrowLeftComponent } from './arrow-left/arrow-left.component';
 import { ArrowRightComponent } from './arrow-right/arrow-right.component';
 import { SlideButtonComponent } from '../../../components/slide-button/slide-button.component';
+import { SlideBarComponent } from '../../../components/slide-bar/slide-bar.component';
 
 @Component({
   selector: 'app-a-hero',
   standalone: true,
-  imports: [HeaderComponent, ArrowLeftComponent, ArrowRightComponent, SlideButtonComponent],
+  imports: [HeaderComponent, ArrowLeftComponent, ArrowRightComponent, SlideButtonComponent, SlideBarComponent],
   templateUrl: './a-hero.component.html',
   styleUrl: './a-hero.component.scss'
 })
