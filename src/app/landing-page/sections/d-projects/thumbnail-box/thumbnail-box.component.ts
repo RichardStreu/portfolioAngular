2891e3e11aaab13e81cfde8ddfa2df9613a603d5
Richard Streu <rstreu13@icloud.com>
2024-12-05 15:41:44 +0100

    feat: enhance ThumbnailBoxComponent with image display and improved positioning logic

--- src/app/landing-page/sections/d-projects/thumbnail-box/thumbnail-box.component.ts
+++ src/app/landing-page/sections/d-projects/thumbnail-box/thumbnail-box.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ProjectDataService } from '../../../../services/project-data.service';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,27 +8,31 @@ import { CommonModule } from '@angular/common';
   templateUrl: './thumbnail-box.component.html',
   styleUrl: './thumbnail-box.component.scss',
 })
+
 export class ThumbnailBoxComponent {
   @Input('projectData') projectData: any;
-  @Input('index') index: number = 0;
+  @Input({ required: true }) index: number = 0;
 
   positionTop: string = '';
+  image: string = '';
 
   mathPositionTop() {
     if (this.index == 0) {
-      this.positionTop = '0px';
+      this.positionTop = '20px';
     } else if (this.index < this.projectData.length - 1) {
       let basicPercent = 100 / (this.projectData.length * 2);
-      let percent = basicPercent + (basicPercent * 2 * this.index);
-      this.positionTop = `calc(${percent}% - 96px)`;
+      let percent = basicPercent + basicPercent * 2 * this.index;
+      this.positionTop = `calc(${percent}% - 76px)`;
     } else {
-      this.positionTop = 'calc(100% - 192px)';
+      this.positionTop = 'calc(100% - 172px)';
     }
   }
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit() {
     this.mathPositionTop();
+    this.image = this.projectData[this.index].projectImg;
   }
 }
