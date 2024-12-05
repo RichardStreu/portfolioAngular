395ad83c4862398d353a3a7a1985d8c1a400bded
Richard Streu <rstreu13@icloud.com>
2024-12-05 16:35:28 +0100

    feat: add thumbnail visibility control with fade-in/out effects in ShowThumbnailService

--- src/app/services/show-thumbnail.service.ts
+++ src/app/services/show-thumbnail.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ElementRef, Renderer2 } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShowThumbnailService {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
-  constructor() { }
+  showThumbnail(index: number) {
+    let thumbnail = this.el.nativeElement.querySelector(
+      `#thumbnailBoxNr${index}`
+    );
+    if (thumbnail) {
+      this.renderer.addClass(thumbnail, 'd_block');
+      setTimeout(() => {
+        this.renderer.addClass(thumbnail, 'opacity_1');
+      }, 20);
+    }
+  }
+
+  hideThumbnail(index: number) {
+    let thumbnail = this.el.nativeElement.querySelector(
+      `#thumbnailBoxNr${index}`
+    );
+    if (thumbnail) {
+      this.renderer.removeClass(thumbnail, 'opacity_1');
+      setTimeout(() => {
+        this.renderer.removeClass(thumbnail, 'd_block');
+      }, 220);
+    }
+  }
 }
