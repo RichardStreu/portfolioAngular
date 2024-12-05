411596322283eb559f496c3651e0747c71004a71
Richard Streu <rstreu13@icloud.com>
2024-12-05 19:17:48 +0100

    feat: integrate ProjectModalService for managing project modal state in DProjectsComponent

--- src/app/landing-page/sections/d-projects/d-projects.component.ts
+++ src/app/landing-page/sections/d-projects/d-projects.component.ts
@@ -4,38 +4,45 @@ import { LanguageService } from '../../../services/language.service';
 import { ProjectBarComponent } from './project-bar/project-bar.component';
 import { ThumbnailBoxComponent } from './thumbnail-box/thumbnail-box.component';
 import { ProjectModalComponent } from '../../../components/project-modal/project-modal.component';
+import { ProjectModalService } from '../../../services/project-modal.service';
 
 @Component({
   selector: 'app-d-projects',
   standalone: true,
   imports: [ProjectBarComponent, ThumbnailBoxComponent, ProjectModalComponent],
   templateUrl: './d-projects.component.html',
-  styleUrl: './d-projects.component.scss'
+  styleUrl: './d-projects.component.scss',
 })
 export class DProjectsComponent {
-
   languageService = inject(LanguageService);
   projectDataService = inject(ProjectDataService);
+  projectModalService = inject(ProjectModalService);
 
-  projectData:any;
+  projectData: any;
 
   featuredText: string = '';
   exploreText: string = '';
 
-  isProjectModalOpen: boolean = false;
-
   chooseLanguage() {
     this.languageService.language.subscribe(() => {
       this.featuredText =
-        this.languageService.currentLanguage == 'en' ? 'Featured Projects' : 'Ausgewählte Projekte';
+        this.languageService.currentLanguage == 'en'
+          ? 'Featured Projects'
+          : 'Ausgewählte Projekte';
       this.exploreText =
-        this.languageService.currentLanguage == 'en' ? 'Explore a selection of my work here - Interact with projects to see my skills in action.' : 'Erkunde hier eine Auswahl meiner Arbeit - Interagiere mit Projekten, um meine Fähigkeiten in Aktion zu sehen.';
+        this.languageService.currentLanguage == 'en'
+          ? 'Explore a selection of my work here - Interact with projects to see my skills in action.'
+          : 'Erkunde hier eine Auswahl meiner Arbeit - Interagiere mit Projekten, um meine Fähigkeiten in Aktion zu sehen.';
     });
   }
 
   constructor() {
-    this.projectData = this.projectDataService.projects
+    this.projectData = this.projectDataService.projects;
     this.chooseLanguage();
+    this.projectModalService.isProjectModalOpen.subscribe((boolean) => {
+      this.isProjectModalOpen = boolean;
+    });
   }
 
+  isProjectModalOpen: boolean = false;
 }
