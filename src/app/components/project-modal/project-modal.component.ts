54d81e0687ae0d0b7df6b7dd4e5a247e7d3c34c3
Richard Streu <rstreu13@icloud.com>
2024-12-05 18:33:26 +0100

    feat: integrate ProjectModalService for dynamic project index handling in ProjectModalComponent

--- src/app/components/project-modal/project-modal.component.ts
+++ src/app/components/project-modal/project-modal.component.ts
@@ -3,20 +3,24 @@ import { LanguageService } from '../../services/language.service';
 import { ProjectDataService } from '../../services/project-data.service';
 import { CommonModule } from '@angular/common';
 import { Project } from '../../shared/interfaces';
+import { BehaviorSubject } from 'rxjs';
+import { ProjectModalService } from '../../services/project-modal.service';
 
 @Component({
   selector: 'app-project-modal',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './project-modal.component.html',
-  styleUrl: './project-modal.component.scss'
+  styleUrl: './project-modal.component.scss',
 })
 export class ProjectModalComponent {
-
   languageService = inject(LanguageService);
-  projectData:any = inject(ProjectDataService);
+  projectData: any = inject(ProjectDataService);
+  projectModalService = inject(ProjectModalService);
 
-  @Input('index') index: number = 0;
+  // @Input('index') index: number = 0;
+
+  index: number = 0;
 
   headlineText: string = '';
   descriptionText: string = '';
@@ -27,7 +31,7 @@ export class ProjectModalComponent {
         this.languageService.currentLanguage == 'en'
           ? this.projectData[this.index].headline.en
           : this.projectData[this.index].headline.de;
-      this.descriptionText = 
+      this.descriptionText =
         this.languageService.currentLanguage == 'en'
           ? this.projectData[this.index].description.en
           : this.projectData[this.index].description.de;
@@ -36,5 +40,8 @@ export class ProjectModalComponent {
 
   constructor() {
     this.chooseLanguage();
+    this.projectModalService.currentProjectIndex.subscribe((index: number) => {
+      this.index = index;
+    });
   }
 }
