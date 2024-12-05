062d00934abda70ccb0e41e39d71efe2b5184346
Richard Streu <rstreu13@icloud.com>
2024-12-05 09:09:09 +0100

    feat: integrate ProjectDataService into ProjectModalComponent and enhance language handling

--- src/app/components/project-modal/project-modal.component.ts
+++ src/app/components/project-modal/project-modal.component.ts
@@ -1,21 +1,37 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input, input } from '@angular/core';
 import { LanguageService } from '../../services/language.service';
+import { ProjectDataService } from '../../services/project-data.service';
+import { CommonModule } from '@angular/common';
+import { Project } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-project-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-modal.component.html',
   styleUrl: './project-modal.component.scss'
 })
 export class ProjectModalComponent {
 
   languageService = inject(LanguageService);
+  projectData:any = inject(ProjectDataService);
 
+  @Input('index') index: number = 0;
 
+  headlineText: string = '';
+  descriptionText: string = '';
 
   chooseLanguage() {
-    this.languageService.language.subscribe(() => {});
+    this.languageService.language.subscribe(() => {
+      this.headlineText =
+        this.languageService.currentLanguage == 'en'
+          ? this.projectData[this.index].headline.en
+          : this.projectData[this.index].headline.de;
+      this.descriptionText = 
+        this.languageService.currentLanguage == 'en'
+          ? this.projectData[this.index].description.en
+          : this.projectData[this.index].description.de;
+    });
   }
 
   constructor() {
