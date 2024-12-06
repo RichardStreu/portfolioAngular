9fc05633eb6b5cbdfed407fbeefa862b3179b77a
Richard Streu <rstreu13@icloud.com>
2024-12-06 10:00:53 +0100

    feat: enhance ProjectModalService with project index management and modal state tracking

--- src/app/services/project-modal.service.ts
+++ src/app/services/project-modal.service.ts
@@ -4,29 +4,56 @@ import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
+/**
+ * Service to manage the state of the project modal and the currently selected project.
+ */
 export class ProjectModalService {
-  constructor() {}
-
+  /**
+   * Indicates whether the project modal is open.
+   */
   isProjectModalOpen: boolean = false;
 
+  /**
+   * Observable to track the state of the project modal.
+   */
   isProjectModalOpen$ = new BehaviorSubject<boolean>(this.isProjectModalOpen);
 
+  /**
+   * Toggles the state of the project modal and updates the observable.
+   * Also sets the current project index.
+   * @param index - The index of the project to be shown.
+   */
   changeIsProjectModalOpen(index: number) {
     this.isProjectModalOpen = !this.isProjectModalOpen;
     this.isProjectModalOpen$.next(this.isProjectModalOpen);
-    this.showClickedProject(index);
+    this.currentProjectIndex.next(index);
   }
 
+  /**
+   * Observable to track the index of the currently selected project.
+   */
   currentProjectIndex = new BehaviorSubject<number>(0);
 
+  /**
+   * Updates the current project index to the specified index.
+   * @param index - The index of the project to be shown.
+   */
   showClickedProject(index: number) {
     this.currentProjectIndex.next(index);
   }
 
+  /**
+   * Updates the current project index to the specified index.
+   * @param index - The index of the next project to be shown.
+   */
   showNextProject(index: number) {
     this.currentProjectIndex.next(index);
   }
 
+  /**
+   * Updates the current project index to the specified index.
+   * @param index - The index of the previous project to be shown.
+   */
   showPreviousProject(index: number) {
     this.currentProjectIndex.next(index);
   }
