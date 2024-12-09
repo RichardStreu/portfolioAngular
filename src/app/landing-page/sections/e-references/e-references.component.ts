a2b18cfc54fd8525d2d8a49eb1e1f3258683741c
Richard Streu <rstreu13@icloud.com>
2024-12-09 15:53:29 +0100

    feat: implement carousel navigation and enhance styling in EReferencesComponent

--- src/app/landing-page/sections/e-references/e-references.component.ts
+++ src/app/landing-page/sections/e-references/e-references.component.ts
@@ -3,22 +3,32 @@ import { Component, inject } from '@angular/core';
 import { LanguageService } from '../../../services/language.service';
 import { ReferenceDataService } from '../../../services/reference-data.service';
 
-
 @Component({
   selector: 'app-e-references',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './e-references.component.html',
-  styleUrl: './e-references.component.scss'
+  styleUrl: './e-references.component.scss',
 })
 export class EReferencesComponent {
-
   referenceDataService = inject(ReferenceDataService);
   languageService = inject(LanguageService);
 
   references: any = [];
+  carouselArray: any = [];
+
+  currentReferenceIndex: number = 0;
 
   headlineText: string = '';
+
+  constructor() {
+    this.chooseLanguage();
+    this.references = this.referenceDataService.references;
+  }
+
+  ngOnInit() {
+    this.generateCarouselArray();
+  }
 
   chooseLanguage() {
     this.languageService.language.subscribe(() => {
@@ -29,13 +39,6 @@ export class EReferencesComponent {
     });
   }
 
-  constructor() {
-    this.chooseLanguage();
-    this.references = this.referenceDataService.references;
-  }
-
-  ngOnInit() {}
-
   generateCarouselArray() {
     let carouselArray = [
       this.references[this.references.length - 1],
@@ -43,6 +46,26 @@ export class EReferencesComponent {
       this.references[0],
       this.references[1],
     ];
+    this.carouselArray = carouselArray;
   }
 
+  getElementSizes() {
+
+  }
+
+  previousSlide() {
+    if (this.currentReferenceIndex > 0) {
+      this.currentReferenceIndex--;
+    } else {
+      this.currentReferenceIndex = this.references.length - 1;
+    }
+  }
+
+  nextSlide() {
+    if (this.currentReferenceIndex < this.references.length - 1) {
+      this.currentReferenceIndex++;
+    } else {
+      this.currentReferenceIndex = 0;
+    }
+  }
 }
