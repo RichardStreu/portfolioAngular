1702845bd8bcfaec190d46f87c71e5ecdf1af09c
Richard Streu <rstreu13@icloud.com>
2024-12-09 11:47:53 +0100

    feat: initialize references in ReferenceDataService with sample data

--- src/app/services/reference-data.service.ts
+++ src/app/services/reference-data.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Reference } from '../shared/interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReferenceDataService {
+  references: Reference[] = [];
 
-  constructor() { }
+  constructor() {
+    this.references = [
+      {
+        name: 'Max Mustermann',
+        position: 'CEO',
+        referenceText:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+      },
+      {
+        name: 'Max Mustermann',
+        position: 'CEO',
+        referenceText:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+      },
+      {
+        name: 'Max Mustermann',
+        position: 'CEO',
+        referenceText:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+      },
+    ];
+  }
 }
