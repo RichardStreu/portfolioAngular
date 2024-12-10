b1479bc7ce9f1ded997873b4c8110236e611d68c
Richard Streu <rstreu13@icloud.com>
2024-12-10 15:49:06 +0100

    feat: redesign contact form with dynamic language support and privacy policy integration

--- src/app/landing-page/sections/f-contact/contact-form/contact-form.component.ts
+++ src/app/landing-page/sections/f-contact/contact-form/contact-form.component.ts
@@ -1,46 +1,88 @@
 import { Component, inject } from '@angular/core';
 import { LanguageService } from '../../../../services/language.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrl: './contact-form.component.scss',
 })
 export class ContactFormComponent {
   languageService = inject(LanguageService);
 
-  whatsNameText: string = '';
-  exampleNameText: string = '';
-  nameValidationText: string = '';
+  labelNameText: string = '';
+  placeholderNameText: string = '';
+  nameInputValue: string = '';
 
-  whatsEmailText: string = '';
-  exampleEmailText: string = '';
-  emailValidationText: string = '';
+  labelEmailText: string = '';
+  placeholderEmailText: string = '';
+  emailInputValue: string = '';
 
-  whatsMessageText: string = '';
-  exampleMessageText: string = '';
-  messageValidationText: string = '';
-  
-  privacyPolicyText: string = '';
+  labelMessageText: string = '';
+  placeholderMessageText: string = '';
+  messageInputValue: string = '';
+
+  privacyPolicyFirstText: string = '';
+  privacyPolicyLinkText: string = '';
+  privacyPolicySecondText: string = '';
   privacyPolicyValidationText: string = '';
+
   submitButtonText: string = '';
 
   chooseLanguage() {
     this.languageService.language.subscribe(() => {
-      this.whatsNameText =
+      this.labelNameText =
         this.languageService.currentLanguage == 'en'
           ? "What's your name?"
           : 'Wie ist dein Name?';
-      this.exampleNameText =
+      this.placeholderNameText =
         this.languageService.currentLanguage == 'en'
           ? 'Your name goes here'
           : 'Dein Name hier';
+      this.labelEmailText =
+        this.languageService.currentLanguage == 'en'
+          ? "What's your email?"
+          : 'Wie ist deine E-Mail?';
+      this.placeholderEmailText =
+        this.languageService.currentLanguage == 'en'
+          ? 'youremail@email.com'
+          : 'deinemail@email.com';
+      this.labelMessageText =
+        this.languageService.currentLanguage == 'en'
+          ? 'How can I help you?'
+          : 'Wie kann ich dir helfen?';
+      this.placeholderMessageText =
+        this.languageService.currentLanguage == 'en'
+          ? 'Hello Richard, I am interested in...'
+          : 'Hallo Richard, ich bin interessiert an...';
+      this.privacyPolicyFirstText =
+        this.languageService.currentLanguage == 'en'
+          ? "I've read the "
+          : 'Ich habe die ';
+      this.privacyPolicyLinkText =
+        this.languageService.currentLanguage == 'en'
+          ? 'privacy policy '
+          : 'Datenschutzerklärung ';
+      this.privacyPolicySecondText =
+        this.languageService.currentLanguage == 'en'
+          ? 'and agree to the processing of my data as outlined.'
+          : 'gelesen und stimme der Verarbeitung meiner Daten zu.';
+      this.submitButtonText =
+        this.languageService.currentLanguage == 'en'
+          ? 'Say Hello ;)'
+          : 'Sag Hallo ;)';
     });
   }
 
   constructor() {
     this.chooseLanguage();
   }
+
+  ngOnInit() {
+    this.chooseLanguage();
+  }
+
+  onSubmit() {}
 }
