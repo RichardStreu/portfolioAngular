35db1b8a74684990b3f8e6a9979fbada0503a23c
Richard Streu <rstreu13@icloud.com>
2025-01-03 12:01:03 +0100

    fix: correct styleUrl syntax in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [RouterOutlet, FooterComponent, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolioAngular';
