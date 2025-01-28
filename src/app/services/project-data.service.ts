fb4e7da50117647a2f7f39d04f313c16856fb413
Richard Streu <rstreu13@icloud.com>
2025-01-28 13:38:47 +0100

    fix: add line break in project modal and uncomment skills in project data service

--- src/app/services/project-data.service.ts
+++ src/app/services/project-data.service.ts
@@ -41,8 +41,8 @@ export class ProjectDataService {
         projectImg: 'sharky.png',
         skillsList: [
           { skillName: 'JavaScript', skillImg: 'JavaScript.svg' },
-          // { skillName: 'SCSS', skillImg: 'CSS.svg' },
-          // { skillName: 'HTML', skillImg: 'HTML.svg' },
+          { skillName: 'SCSS', skillImg: 'CSS.svg' },
+          { skillName: 'HTML', skillImg: 'HTML.svg' },
         ],
         headline: {
           en: 'What is this project about?',
@@ -57,74 +57,74 @@ export class ProjectDataService {
           live: 'https://sharky.richardstreu.dev/',
         },
       },
-      {
-        name: 'DEVerando',
-        isCurrentlyShown: false,
-        projectImg: 'deverando.png',
-        skillsList: [
-          { skillName: 'JavaScript', skillImg: 'JavaScript.svg' },
-          { skillName: 'CSS', skillImg: 'CSS.svg' },
-          { skillName: 'HTML', skillImg: 'HTML.svg' },
-        ],
-        headline: {
-          en: 'What is this project about?',
-          de: 'Worum geht es in diesem Projekt?',
-        },
-        description: {
-          en: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
-          de: 'Aufgabenmanager inspiriert vom Kanban System. Erstelle und organisiere Aufgaben mit Drag and Drop Funktionen, weise Benutzer und Kategorien zu.',
-        },
-        links: {
-          github: 'https://github.com/RichardStreu/LieferDEVo',
-          live: 'https://deverando.richardstreu.dev/',
-        },
-      },
-      {
-        name: 'Pokedex',
-        isCurrentlyShown: false,
-        projectImg: 'pokedex.png',
-        skillsList: [
-          { skillName: 'REST', skillImg: 'Rest-Api.svg' },
-          { skillName: 'JavaScript', skillImg: 'JavaScript.svg' },
-          { skillName: 'CSS', skillImg: 'CSS.svg' },
-          { skillName: 'HTML', skillImg: 'HTML.svg' },
-        ],
-        headline: {
-          en: 'What is this project about?',
-          de: 'Worum geht es in diesem Projekt?',
-        },
-        description: {
-          en: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
-          de: 'Aufgabenmanager inspiriert vom Kanban System. Erstelle und organisiere Aufgaben mit Drag and Drop Funktionen, weise Benutzer und Kategorien zu.',
-        },
-        links: {
-          github: 'https://github.com/RichardStreu/Developer_Akademie-Pokedex',
-          live: 'https://pokedex.richardstreu.dev/',
-        },
-      },
-      {
-        name: 'Nordic Impressions',
-        isCurrentlyShown: false,
-        projectImg: 'photoGallery.png',
-        skillsList: [
-          { skillName: 'JavaScript', skillImg: 'JavaScript.svg' },
-          { skillName: 'CSS', skillImg: 'CSS.svg' },
-          { skillName: 'HTML', skillImg: 'HTML.svg' },
-        ],
-        headline: {
-          en: 'What is this project about?',
-          de: 'Worum geht es in diesem Projekt?',
-        },
-        description: {
-          en: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
-          de: 'Aufgabenmanager inspiriert vom Kanban System. Erstelle und organisiere Aufgaben mit Drag and Drop Funktionen, weise Benutzer und Kategorien zu.',
-        },
-        links: {
-          github:
-            'https://github.com/RichardStreu/Developer_Akademie-Nordic_Impressions',
-          live: 'https://nordic-impressions.richardstreu.dev/',
-        },
-      },
+      // {
+      //   name: 'DEVerando',
+      //   isCurrentlyShown: false,
+      //   projectImg: 'deverando.png',
+      //   skillsList: [
+      //     { skillName: 'JavaScript', skillImg: 'JavaScript.svg' },
+      //     { skillName: 'CSS', skillImg: 'CSS.svg' },
+      //     { skillName: 'HTML', skillImg: 'HTML.svg' },
+      //   ],
+      //   headline: {
+      //     en: 'What is this project about?',
+      //     de: 'Worum geht es in diesem Projekt?',
+      //   },
+      //   description: {
+      //     en: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      //     de: 'Aufgabenmanager inspiriert vom Kanban System. Erstelle und organisiere Aufgaben mit Drag and Drop Funktionen, weise Benutzer und Kategorien zu.',
+      //   },
+      //   links: {
+      //     github: 'https://github.com/RichardStreu/LieferDEVo',
+      //     live: 'https://deverando.richardstreu.dev/',
+      //   },
+      // },
+      // {
+      //   name: 'Pokedex',
+      //   isCurrentlyShown: false,
+      //   projectImg: 'pokedex.png',
+      //   skillsList: [
+      //     { skillName: 'REST', skillImg: 'Rest-Api.svg' },
+      //     { skillName: 'JavaScript', skillImg: 'JavaScript.svg' },
+      //     { skillName: 'CSS', skillImg: 'CSS.svg' },
+      //     { skillName: 'HTML', skillImg: 'HTML.svg' },
+      //   ],
+      //   headline: {
+      //     en: 'What is this project about?',
+      //     de: 'Worum geht es in diesem Projekt?',
+      //   },
+      //   description: {
+      //     en: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      //     de: 'Aufgabenmanager inspiriert vom Kanban System. Erstelle und organisiere Aufgaben mit Drag and Drop Funktionen, weise Benutzer und Kategorien zu.',
+      //   },
+      //   links: {
+      //     github: 'https://github.com/RichardStreu/Developer_Akademie-Pokedex',
+      //     live: 'https://pokedex.richardstreu.dev/',
+      //   },
+      // },
+      // {
+      //   name: 'Nordic Impressions',
+      //   isCurrentlyShown: false,
+      //   projectImg: 'photoGallery.png',
+      //   skillsList: [
+      //     { skillName: 'JavaScript', skillImg: 'JavaScript.svg' },
+      //     { skillName: 'CSS', skillImg: 'CSS.svg' },
+      //     { skillName: 'HTML', skillImg: 'HTML.svg' },
+      //   ],
+      //   headline: {
+      //     en: 'What is this project about?',
+      //     de: 'Worum geht es in diesem Projekt?',
+      //   },
+      //   description: {
+      //     en: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      //     de: 'Aufgabenmanager inspiriert vom Kanban System. Erstelle und organisiere Aufgaben mit Drag and Drop Funktionen, weise Benutzer und Kategorien zu.',
+      //   },
+      //   links: {
+      //     github:
+      //       'https://github.com/RichardStreu/Developer_Akademie-Nordic_Impressions',
+      //     live: 'https://nordic-impressions.richardstreu.dev/',
+      //   },
+      // },
     ];
 
     this.projectsLength = this.projects.length;
