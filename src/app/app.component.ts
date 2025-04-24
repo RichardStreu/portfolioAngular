78b9c3e4615a594f62bbcb533b0729eef843d881
Richard Streu <rstreu13@icloud.com>
2025-04-24 09:23:08 +0200

    fix: refactor app component by improving variable visibility and cleaning up comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,39 +13,33 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'portfolioAngular';
+  window: any = window;
 
-  // Boolean to track the visibility of the burger menu.
-  isBurgerMenuVisible: boolean = false;
-
-  // Method to set the visibility of the burger menu.
   setBurgerMenuVisibility(boolean: boolean) {
     this.isBurgerMenuVisible = boolean;
   }
 
-  // Boolean to track if the header is hidden.
-  isHeaderHidden = false;
+  public isBurgerMenuVisible: boolean = false;
 
-  // Variable to store the last scroll position.
-  private lastScrollPosition = 0;
+  public isHeaderHidden: boolean = false;
 
-  // Reference to the window object.
-  window: any = window;
+  public isScrolling: boolean = false;
 
-  // Boolean to track if the user is scrolling.
-  isScrolling: boolean = false;
+  private lastScrollPosition: number = 0;
+  private lastToggleScrollY: number = 0;
+  private toggleThreshold: number = 25;
 
   // HostListener to handle window scroll events.
   @HostListener('window:scroll', [])
   showHideHeader() {
-    // Check if the scroll position is greater than 100 and the burger menu is not visible.
+
     if (this.window.scrollY > 100 && !this.isBurgerMenuVisible) {
-      // Hide the header if the user is scrolling down, otherwise show it.
       if (this.window.scrollY > this.lastScrollPosition) {
         this.isHeaderHidden = true;
       } else {
         this.isHeaderHidden = false;
       }
-      // Update the last scroll position.
+
       this.lastScrollPosition = this.window.scrollY;
     }
   }
