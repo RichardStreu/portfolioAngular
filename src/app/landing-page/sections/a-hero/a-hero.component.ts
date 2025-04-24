16fbb535d6bd737b4c077390015f39b2b9675fb8
Richard Streu <rstreu13@icloud.com>
2025-04-24 10:19:33 +0200

    fix: remove unused HeaderComponent import from AHeroComponent

--- src/app/landing-page/sections/a-hero/a-hero.component.ts
+++ src/app/landing-page/sections/a-hero/a-hero.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { HeaderComponent } from '../../../core/header/header.component';
 import { ArrowLeftComponent } from './arrow-left/arrow-left.component';
 import { ArrowRightComponent } from './arrow-right/arrow-right.component';
 import { SlideButtonComponent } from '../../../components/slide-button/slide-button.component';
@@ -11,7 +10,6 @@ import { CommonModule } from '@angular/common';
   selector: 'app-a-hero',
   standalone: true,
   imports: [
-    HeaderComponent,
     ArrowLeftComponent,
     ArrowRightComponent,
     SlideButtonComponent,
