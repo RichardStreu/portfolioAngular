450e36a3a0bd2d1916b1d17e554ca5c48947c558
Richard Streu <rstreu13@icloud.com>
2025-04-24 09:31:06 +0200

    fix: improve scroll handling logic in app component for better header visibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,22 +25,25 @@ export class AppComponent {
 
   public isScrolling: boolean = false;
 
-  private lastScrollPosition: number = 0;
-  private lastToggleScrollY: number = 0;
-  private toggleThreshold: number = 25;
+  private lastToggleScrollY: number = 60;
+
+  private toggleThreshold: number = 40;
 
   // HostListener to handle window scroll events.
   @HostListener('window:scroll', [])
   showHideHeader() {
 
     if (this.window.scrollY > 100 && !this.isBurgerMenuVisible) {
-      if (this.window.scrollY > this.lastScrollPosition) {
+
+
+      if (this.window.scrollY > (this.lastToggleScrollY + this.toggleThreshold)) {
         this.isHeaderHidden = true;
-      } else {
+        this.lastToggleScrollY = this.window.scrollY;
+      } else if (this.window.scrollY < (this.lastToggleScrollY - this.toggleThreshold)) {
         this.isHeaderHidden = false;
+        this.lastToggleScrollY = this.window.scrollY;
       }
 
-      this.lastScrollPosition = this.window.scrollY;
     }
   }
 }
